66defb2276119b9a89380ce08f6d6fababf57c74
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-01-21 01:00:32 +0100

    adding auth

--- frontend-epdn/src/App.js
+++ frontend-epdn/src/App.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './auth/login';
+import Login from './components/auth/login';
+import Acceuil from './components/dashboard/Acceuil'
+import useToken from './components/auth/useToken';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div class="App">
-      <Login />
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/acceuil">
+            <Acceuil />
+          </Route>
+          <Route path="/">
+            <h2>default page</h2>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
